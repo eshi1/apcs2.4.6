51202ed61b4e9b52ee26261b9f188acf38db0794
Erica S <doobird12@gmail.com>
2022-11-03 12:55:54 +0000

    cs

---
+++ ConsumerLab/Main.java
@@ -0,0 +1,11 @@
+package ConsumerLab;
+/*import java.util.Scanner;
+import java.io.File;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+*/
+public class Main {
+    Review rv = new Review();
+}
